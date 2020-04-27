cf17df6113408028f8cc0ca9523b198e9a1e1515
张磊 <codelei@163.com>
2020-04-27 11:28:20 +0800

    初始完成项目，部分功能待完善。

---
+++ 抽奖机/抽奖机/FormMain.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace 抽奖机
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.作者张磊ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.微信QQ2213407066ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.本程序无毒无害敬请放心使用ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.点击按钮或按回车键开始抽奖ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("黑体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(114, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(388, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "结束";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("黑体", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(124, 200);
+            this.label1.MinimumSize = new System.Drawing.Size(620, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(620, 99);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文字显示区域";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripTextBox1,
+            this.toolStripSeparator2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(879, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.作者张磊ToolStripMenuItem,
+            this.微信QQ2213407066ToolStripMenuItem,
+            this.本程序无毒无害敬请放心使用ToolStripMenuItem});
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(69, 22);
+            this.toolStripTextBox1.Text = "关于程序";
+            // 
+            // 作者张磊ToolStripMenuItem
+            // 
+            this.作者张磊ToolStripMenuItem.Name = "作者张磊ToolStripMenuItem";
+            this.作者张磊ToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.作者张磊ToolStripMenuItem.Text = "作者：张磊";
+            // 
+            // 微信QQ2213407066ToolStripMenuItem
+            // 
+            this.微信QQ2213407066ToolStripMenuItem.Name = "微信QQ2213407066ToolStripMenuItem";
+            this.微信QQ2213407066ToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.微信QQ2213407066ToolStripMenuItem.Text = "微信/QQ：2213407066";
+            // 
+            // 本程序无毒无害敬请放心使用ToolStripMenuItem
+            // 
+            this.本程序无毒无害敬请放心使用ToolStripMenuItem.Name = "本程序无毒无害敬请放心使用ToolStripMenuItem";
+            this.本程序无毒无害敬请放心使用ToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.本程序无毒无害敬请放心使用ToolStripMenuItem.Text = "本程序无毒无害，敬请放心使用...";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(173, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(517, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(18, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(508, 20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(124, 330);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(620, 124);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.点击按钮或按回车键开始抽奖ToolStripMenuItem});
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(69, 22);
+            this.toolStripLabel1.Text = "使用帮助";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // 点击按钮或按回车键开始抽奖ToolStripMenuItem
+            // 
+            this.点击按钮或按回车键开始抽奖ToolStripMenuItem.Name = "点击按钮或按回车键开始抽奖ToolStripMenuItem";
+            this.点击按钮或按回车键开始抽奖ToolStripMenuItem.Size = new System.Drawing.Size(244, 22);
+            this.点击按钮或按回车键开始抽奖ToolStripMenuItem.Text = "点击按钮或按回车键开始抽奖。";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 612);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(855, 31);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "当前时间";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 460);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(855, 147);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "状态消息";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(849, 127);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(879, 652);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(895, 535);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "抽奖机 ---- 版本：Bate v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem 作者张磊ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 微信QQ2213407066ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 本程序无毒无害敬请放心使用ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripLabel1;
+        private System.Windows.Forms.ToolStripMenuItem 点击按钮或按回车键开始抽奖ToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ 抽奖机/抽奖机/FormMain.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Media;
+
+namespace 抽奖机
+{
+    public partial class FormMain : Form
+    {
+        //随机显示文本文件内的姓名
+        string[] name = { "张三","李四","王五","赵六","徐七" };
+        //记录空值
+        int count = 1;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //用于显示按钮中文字
+            if (button1.Text == "开始")
+            {
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (name[i].Equals(label1.Text))        //如果姓名数组[i]=当前输出文本
+                    {
+                        name[i] = "";           //姓名数组[i]=空文本
+                        break;                     //退出循环
+                    }
+                }
+                count++;                            //记录不为空的数值
+                button1.Text = "结束"; timer1.Start();
+            }
+            else
+            {
+                //更新状态栏信息
+                richTextBox1.Text = richTextBox1.Text + "第" + count + "名中奖用户是:" + label1.Text + "\n";
+                button1.Text = "开始"; timer1.Stop();
+            }
+
+            //当仅剩最后一个人时提示抽奖结束并禁用控件
+            if (count == name.Length)
+            {
+                //循环查找最后一个不为空的姓名并添加
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (!(name[i].Equals("")))
+                    {
+                        //更新最后一名中奖人员状态栏信息
+                        richTextBox1.Text = richTextBox1.Text + "第" + count + "名中奖用户是:" + name[i] + "\n";
+                        break;
+                    }
+                }
+                button1.Text = "抽奖结束";
+                button1.Enabled = false;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //随机显示字符串数组文字
+            Random r = new Random();
+            int i = r.Next(0, name.Length);
+
+            for (int j=0;j<name.Length;j++)
+            {
+                if (name[i].Equals(""))                //如果姓名数组[i]=空文本
+                {
+                    continue;                             //继续循环查找
+                }
+                else
+                {
+                    label1.Text = name[i];          //输出当前文本
+                    break;                                  //退出循环
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //主窗口加载时计时器开始
+            timer1.Start();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.label2.Text = DateTime.Now.ToString();
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //用于显示按钮中文字
+                if (button1.Text == "开始")
+                {
+                    for (int i = 0; i < name.Length; i++)
+                    {
+                        if (name[i].Equals(label1.Text))        //如果姓名数组[i]=当前输出文本
+                        {
+                            name[i] = "";           //姓名数组[i]=空文本
+                            break;                     //退出循环
+                        }
+                    }
+                    count++;                            //记录不为空的数值
+                    button1.Text = "结束"; timer1.Start();
+
+                    //当仅剩最后一个人时提示抽奖结束并禁用控件
+                    if (count == name.Length)
+                    {
+                        //循环查找最后一个不为空的姓名并添加
+                        for (int i=0;i<name.Length;i++)
+                        {
+                            if (!(name[i].Equals("")))
+                            {
+                                //更新最后一名中奖人员状态栏信息
+                                richTextBox1.Text = richTextBox1.Text + "第" + count + "名中奖用户是:" + name[i] + "\n";
+                                break;
+                            }
+                        }
+
+                        button1.Text = "抽奖结束";
+                        button1.Enabled = false;
+                    }
+                }
+                else
+                {
+                    //更新状态栏信息
+                    richTextBox1.Text = richTextBox1.Text + "第" + count + "名中奖用户是:" + label1.Text + "\n";
+                    button1.Text = "开始"; timer1.Stop();
+                }
+
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
